f81849b850a4e821eca0c73c9df39c688cb10114
Gilles Leblanc <gilles.leblanc@gmail.com>
2017-10-16 21:38:00 -0400

    Add modal handling for plants and borders

--- TomatoStakeWeb/src/app/authenticated/garden-planner/garden-planner.component.ts
+++ TomatoStakeWeb/src/app/authenticated/garden-planner/garden-planner.component.ts
@@ -1,12 +1,13 @@
 // The agenda/calendar component which displays a list of garden jobs
 
 import { Component } from '@angular/core';
+import { ModalHandler } from '../modal';
 
 @Component({
   selector: 'planner',
   templateUrl: './garden-planner.component.html',
   styleUrls: [ './garden-planner.component.css' ]
 })
-export class GardenPlannerComponent {
+export class GardenPlannerComponent extends ModalHandler {
   
 }
