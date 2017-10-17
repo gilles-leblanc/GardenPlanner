cd4e7e80526131c6b7812b64f8c27ba028d793a5
Gilles Leblanc <gilles.leblanc@gmail.com>
2017-10-16 21:30:43 -0400

    Add modal code to plants-borders

--- TomatoStakeWeb/src/app/authenticated/plants-borders/plants-borders.component.ts
+++ TomatoStakeWeb/src/app/authenticated/plants-borders/plants-borders.component.ts
@@ -1,6 +1,6 @@
 // The plants and borders component which displays the user's garden borders and plants
 
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'plants-borders',
@@ -8,5 +8,68 @@ import { Component } from '@angular/core';
   // styleUrls: [ './landing.component.css' ]
 })
 export class PlantsBordersComponent {
+  isModalVisible: boolean;
   
+  openModal(): void {
+    this.isModalVisible = true;
+  }  
+
+  closeModal(): void {
+    this.isModalVisible = false;
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  keyboardInput(event: KeyboardEvent) {
+    this.closeModal();
+  }
 }
+
+
+
+// // Modals
+
+// var $html = document.documentElement;
+// var $modals = getAll('.modal');
+// var $modalButtons = getAll('.modal-button');
+// var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+
+// if ($modalButtons.length > 0) {    
+//     $modalButtons.forEach(function ($el) {        
+//         $el.addEventListener('click', function () {
+//             var target = $el.dataset.target;
+//             var $target = document.getElementById(target);
+//             $html.classList.add('is-clipped');
+//             $target.classList.add('is-active');
+//         });
+//     });
+// }
+
+// if ($modalCloses.length > 0) {
+//     $modalCloses.forEach(function ($el) {
+//         $el.addEventListener('click', function () {
+//             closeModals();
+//         });
+//     });
+// }
+
+// document.addEventListener('keydown', function (event) {
+//     var e = event || window.event;
+//     if (e.keyCode === 27) {
+//         closeModals();
+//         closeDropdowns();
+//     }
+// });
+
+// function closeModals() {
+//     $html.classList.remove('is-clipped');
+//     $modals.forEach(function ($el) {
+//         $el.classList.remove('is-active');
+//     });
+// }
+
+
+// // Functions
+
+// function getAll(selector) {
+//     return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+// }
